b32b0a4251029f4337996017b1e74d80c9e30448
Anticoder03 <ap5381545@gmail.com>
2026-05-10 09:49:02 +0530

    updtae app

--- app.py
+++ app.py
@@ -9,9 +9,10 @@ from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 
 # =========================
-# DOWNLOAD REQUIRED DATA
+# DOWNLOAD NLTK DATA
 # =========================
 nltk.download('punkt')
+nltk.download('punkt_tab')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
 nltk.download('stopwords')
